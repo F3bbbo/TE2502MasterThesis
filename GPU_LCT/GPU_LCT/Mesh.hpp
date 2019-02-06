b2d4ad16b551c73fb6b592cfa85289a27510bc12
Simon <gummessonsimon@gmail.com>
2019-02-06 13:48:30 +0100

    Add vertex and edge information

--- GPU_LCT/GPU_LCT/Mesh.hpp
+++ GPU_LCT/GPU_LCT/Mesh.hpp
@@ -2,6 +2,7 @@
 #define MESH_HPP
 
 #include <vector>
+#include <utility>
 #include <glm/glm.hpp>
 
 #include "data_structures.hpp"
@@ -14,8 +15,8 @@ public:
 	~Mesh();
 	void Initialize_as_quad(glm::vec2 scale, glm::vec2 translate);
 private:
-	std::vector<glm::vec2> m_vertices;
-	std::vector<glm::ivec2> m_edges;
+	std::vector<std::pair<glm::vec2, int>> m_vertices; // Each vertice keeps track of how many times it is referenced
+	std::vector<std::pair<glm::ivec2, std::vector<int>>> m_edges; // Edges keeps track of the constraints it represents
 	std::vector<glm::ivec3> m_faces;
 };
 
