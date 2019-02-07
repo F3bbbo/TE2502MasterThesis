1de7ef58ab9b2b1822eda0ee9a56b9e66ffa599f
SimonGummesson <gummessonsimon@gmail.com>
2019-02-07 16:38:53 +0100

    Add renderer base

---
+++ GPU_LCT/GPU_LCT/Renderer.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+#include <glad/glad.h>
+#include <map>
+#include "Pipeline.h"
+
+class Renderer
+{
+public:
+	Renderer();
+	~Renderer();
+private:
+	
+};
+
+#endif
