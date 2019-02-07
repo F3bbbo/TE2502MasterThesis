5417d9a7af34ae21a49dae68a68b174aade6dc44
Unknown <mick3_70@hotmail.com>
2019-02-07 10:56:14 +0100

    Add face struct

--- GPU_LCT/GPU_LCT/Mesh.hpp
+++ GPU_LCT/GPU_LCT/Mesh.hpp
@@ -13,10 +13,16 @@ struct VertexRef
 	int ref_counter;
 };
 
-struct EdgeConstraints
+struct Edge
 {
 	glm::ivec2 edge;
 	std::vector<int> constraint_ref;
+};
+
+struct Face
+{
+	glm::ivec3 vert_i;
+	unsigned int explored = 0; //number indicating last iteration being explored
 };
 
 class Mesh
@@ -28,8 +34,8 @@ public:
 	void Initialize_as_quad(glm::vec2 scale, glm::vec2 translate);
 private:
 	std::vector<VertexRef> m_vertices; // Each vertice keeps track of how many times it is referenced
-	std::vector<EdgeConstraints> m_edges; // Edges keeps track of the constraints it represents
-	std::vector<glm::ivec3> m_faces; // Indices to vertices that makes up each face
+	std::vector<Edge> m_edges; // Edges keeps track of the constraints it represents
+	std::vector<Face> m_faces; // Indices to vertices that makes up each face
 };
 
 #endif
