66fbfd99f813b242628240d177e072eeee9bcbf5
Unknown <mick3_70@hotmail.com>
2019-04-09 15:20:05 +0200

    Add automatic scaling
    Added automatic scaling of the test map, based on how many objects are added to the map.

--- GPU_LCT/GPU_LCT/TestMap.hpp
+++ GPU_LCT/GPU_LCT/TestMap.hpp
@@ -14,12 +14,10 @@ public:
 	std::pair < std::vector<glm::vec2>, std::vector<glm::ivec2>> get_GPU_obstacles();
 	void set_num_obsticles(glm::ivec2 num);
 	void set_map_size(glm::vec2 start, glm::vec2 end);
-	void set_obsticle_scale(float scale);
 private:
 	void clear_shapes();
 	bool m_dirty = true;
 	glm::ivec2 m_num_obsticles;
-	float m_obsticles_scale;
 	glm::vec2 m_start;
 	glm::vec2 m_end;
 	std::vector<Shape*> m_shapes;
