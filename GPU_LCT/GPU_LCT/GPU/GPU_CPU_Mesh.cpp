2e7b69dae8e82d563d16767b804c8561ae08e10a
Unknown <mick3_70@hotmail.com>
2019-04-30 14:25:34 +0200

    Add insert_in_edge_program

--- GPU_LCT/GPU_LCT/GPU/GPU_CPU_Mesh.cpp
+++ GPU_LCT/GPU_LCT/GPU/GPU_CPU_Mesh.cpp
@@ -1133,6 +1133,161 @@ namespace GPU
 	}
 	void GCMesh::insert_in_edge_program()
 	{
+		for (int index = 0; index < point_positions.size(); index++)
+		{
+			if (tri_ins_point_index[index] >= 0)
+			{
+				status = 1;
+				int point_index = tri_ins_point_index[index];
+				point_inserted[point_index] = 1;
+				tri_ins_point_index[index] = -1;
+
+				int segment = tri_symedges[index].x;
+
+				for (int i = 0; i < 2; i++)
+				{
+					if (point_line_test(get_vertex(point_index),
+						get_vertex(get_symedge(segment).vertex),
+						get_vertex(get_symedge(nxt(segment)).vertex)))
+						break;
+					segment = nxt(segment);
+				}
+
+				int e1 = nxt(segment);
+				int e2 = prev(segment);
+
+				int e1_sym = sym(e1);
+				int e2_sym = sym(e2);
+
+				ivec2 segment_symedges = ivec2(segment, rot(nxt(segment)));
+				int new_symedges[6];
+
+				new_symedges[0] = symedge_buffer_size - 6 * (point_positions.size() - point_index);
+				new_symedges[1] = symedge_buffer_size - 6 * (point_positions.size() - point_index) + 1;
+				new_symedges[2] = symedge_buffer_size - 6 * (point_positions.size() - point_index) + 2;
+				new_symedges[3] = symedge_buffer_size - 6 * (point_positions.size() - point_index) + 3;
+				new_symedges[4] = symedge_buffer_size - 6 * (point_positions.size() - point_index) + 4;
+				new_symedges[5] = symedge_buffer_size - 6 * (point_positions.size() - point_index) + 5;
+
+				int t0 = get_symedge(segment_symedges[0]).face;
+				int t1 = tri_seg_inters_index.size() - 2 * (point_positions.size() - point_index);
+				int t2 = tri_seg_inters_index.size() - 2 * (point_positions.size() - point_index) + 1;
+				int t3 = get_symedge(segment_symedges[1]).face;
+
+				int edge1 = edge_label.size() - 3 * (point_positions.size() - point_index);
+				int edge2 = edge_label.size() - 3 * (point_positions.size() - point_index) + 1;
+				int edge3 = edge_label.size() - 3 * (point_positions.size() - point_index) + 2;
+
+				edge_label[edge1] = 0;
+				edge_label[edge2] = 0;
+				edge_label[edge3] = 0;
+
+				// e1
+				sym_edges[e1].nxt = new_symedges[2];
+				sym_edges[e1].rot = segment_symedges[1];
+
+				// e2
+				sym_edges[e2].nxt = new_symedges[0];
+				sym_edges[e2].rot = new_symedges[2];
+				sym_edges[e2].face = t1;
+
+				// new 0
+				sym_edges[new_symedges[0]].nxt = new_symedges[1];
+				sym_edges[new_symedges[0]].rot = e2_sym;
+				sym_edges[new_symedges[0]].vertex = get_symedge(segment_symedges[0]).vertex;
+				sym_edges[new_symedges[0]].edge = edge1;
+				sym_edges[new_symedges[0]].face = t1;
+
+				// new 1
+				sym_edges[new_symedges[1]].nxt = e2;
+				sym_edges[new_symedges[1]].rot = -1; // this will be fixed later if sym(seg) is not null
+				sym_edges[new_symedges[1]].vertex = point_index;
+				sym_edges[new_symedges[1]].edge = edge2;
+				sym_edges[new_symedges[1]].face = t1;
+
+				// new 2
+				sym_edges[new_symedges[2]].nxt = segment_symedges[0];
+				sym_edges[new_symedges[2]].rot = e1_sym;
+				sym_edges[new_symedges[2]].vertex = get_symedge(e2).vertex;
+				sym_edges[new_symedges[2]].edge = edge2;
+				sym_edges[new_symedges[2]].face = t0;
+
+				// old segment 0
+				sym_edges[segment_symedges[0]].nxt = e1;
+				sym_edges[segment_symedges[0]].rot = new_symedges[1];
+				sym_edges[segment_symedges[0]].vertex = point_index;
+
+				tri_symedges[t0] = ivec4(segment_symedges[0], e1, new_symedges[2], -1);
+				tri_symedges[t1] = ivec4(new_symedges[1], e2, new_symedges[0], -1);
+
+				int new_segment_index = seg_inserted.size() - (seg_inserted.size() - point_index);
+
+				seg_endpoint_indices[edge_is_constrained[get_symedge(segment).edge]] = ivec2(point_index, get_symedge(e1).vertex);	// reused segment
+				seg_endpoint_indices[new_segment_index] = ivec2(get_symedge(new_symedges[0]).vertex, point_index);		// new segment
+
+				edge_is_constrained[edge1] = new_segment_index;
+
+				// mark as maybe non delauney
+
+				edge_label[get_symedge(e1).edge] = edge_is_constrained[get_symedge(e1).edge] == -1 ? 1 : edge_label[get_symedge(e1).edge];
+				edge_label[get_symedge(e2).edge] = edge_is_constrained[get_symedge(e2).edge] == -1 ? 1 : edge_label[get_symedge(e2).edge];
+
+				if (rot(nxt(segment)) != -1)
+				{
+					int e3 = nxt(sym(segment));
+					int e4 = nxt(nxt(sym(segment)));
+
+					int e3_sym = sym(e3);
+					int e4_sym = sym(e4);
+
+					// new 1 fix
+					sym_edges[new_symedges[1]].rot = new_symedges[5];
+
+					// e3
+					sym_edges[e3].nxt = new_symedges[4];
+					sym_edges[e3].rot = new_symedges[0];
+					sym_edges[e3].face = t2;
+
+					// e4
+					sym_edges[e4].nxt = segment_symedges[1];
+					sym_edges[e4].rot = new_symedges[4];
+
+					// new 3
+					sym_edges[new_symedges[3]].nxt = e4;
+					sym_edges[new_symedges[3]].rot = segment_symedges[0];
+					sym_edges[new_symedges[3]].vertex = point_index;
+					sym_edges[new_symedges[3]].edge = edge3;
+					sym_edges[new_symedges[3]].face = t3;
+
+					// new 4
+					sym_edges[new_symedges[4]].nxt = new_symedges[5];
+					sym_edges[new_symedges[4]].rot = e3_sym;
+					sym_edges[new_symedges[4]].vertex = get_symedge(e4).vertex;
+					sym_edges[new_symedges[4]].edge = edge3;
+					sym_edges[new_symedges[4]].face = t2;
+
+					// new 5
+					sym_edges[new_symedges[5]].nxt = e3;
+					sym_edges[new_symedges[5]].rot = new_symedges[3];
+					sym_edges[new_symedges[5]].vertex = point_index;
+					sym_edges[new_symedges[5]].edge = edge1;
+					sym_edges[new_symedges[5]].face = t2;
+
+					// old segment 1
+					sym_edges[segment_symedges[1]].nxt = new_symedges[3];
+					sym_edges[segment_symedges[1]].rot = e4_sym;
+
+					tri_symedges[t2] = ivec4(new_symedges[5], e3, new_symedges[4], -1);
+					tri_symedges[t3] = ivec4(new_symedges[3], e4, segment_symedges[1], -1);
+
+					seg_inserted[new_segment_index] = 1;
+
+					// mark as maybe non delauney
+					edge_label[get_symedge(e3).edge] = edge_is_constrained[get_symedge(e3).edge] == -1 ? 1 : edge_label[get_symedge(e3).edge];
+					edge_label[get_symedge(e4).edge] = edge_is_constrained[get_symedge(e4).edge] == -1 ? 1 : edge_label[get_symedge(e4).edge];
+				}
+			}
+		}
 	}
 	void GCMesh::locate_point_triangle_program()
 	{
