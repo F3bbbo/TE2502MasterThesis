e6f6882460699213c7c2969ff1160cdf2883769d
Unknown <mick3_70@hotmail.com>
2019-05-02 14:55:35 +0200

    Updated Flipping_part_one

--- GPU_LCT/GPU_LCT/GPU/flipping_part_one.glsl
+++ GPU_LCT/GPU_LCT/GPU/flipping_part_one.glsl
@@ -110,35 +110,59 @@ void main(void)
 	int index = int(gid);
 	while (index < tri_seg_inters_index.length())
 	{	
-		int highest_priority_s_edge = -1;
-		int h = -1;
-
-		SymEdge edge_sym = get_symedge(tri_symedges[index].x);
-		for (int i = 0; i < 3; i++)
+		if (tri_symedges[index].x > -1)
 		{
-			highest_priority_s_edge = h < edge_label[edge_sym.edge] ? get_index(edge_sym) : highest_priority_s_edge;
-			h = max(edge_label[edge_sym.edge], h);
-			edge_sym = nxt(edge_sym);
-		}
+			int highest_priority_s_edge = -1;
+			int h = -1;
 
-		int sym_symedge = nxt(get_symedge(highest_priority_s_edge)).rot;
-
-		int o_label1 = edge_label[nxt(get_symedge(sym_symedge)).edge];
-		int o_label2 = edge_label[prev_symedge(get_symedge(sym_symedge)).edge];
-
-		if (h > 0 && sym_symedge != -1 && o_label1 != h && o_label2 != h && o_label1 < h && o_label2 < h)
-		{
-			int nh = 0;
+			SymEdge edge_sym = get_symedge(tri_symedges[index].x);
 			for (int i = 0; i < 3; i++)
 			{
-				nh = h == edge_label[edge_sym.edge] ? nh + 1 : nh;
+				highest_priority_s_edge = h < edge_label[edge_sym.edge] ? get_index(edge_sym) : highest_priority_s_edge;
+				h = max(edge_label[edge_sym.edge], h);
 				edge_sym = nxt(edge_sym);
 			}
 
-			if (nh >= 2 || (nh == 1 && index < get_symedge(sym_symedge).face))
-				tri_edge_flip_index[index] = get_symedge(highest_priority_s_edge).edge;
-			else
-				tri_edge_flip_index[index] = -1;
+			if (h > 0)
+			{
+				// calculate nh to know how many other edges the that can be tested
+				int nh = 0;
+				for (int i = 0; i < 3; i++)
+				{
+					nh = h == edge_label[edge_sym.edge] ? nh + 1 : nh;
+					edge_sym = nxt(edge_sym);
+				}
+				int num_iter = nh;
+				while (num_iter > 0)
+				{
+					int sym_symedge = nxt(get_symedge(highest_priority_s_edge)).rot;
+					if (sym_symedge != -1)
+					{
+						int o_label1 = edge_label[nxt(get_symedge(sym_symedge)).edge];
+						int o_label2 = edge_label[prev_symedge(get_symedge(sym_symedge)).edge];
+
+						if (o_label1 != h && o_label2 != h && o_label1 < h && o_label2 < h)
+						{
+							if (nh >= 2 || (nh == 1 && index < get_symedge(sym_symedge).face))
+								tri_edge_flip_index[index] = get_symedge(highest_priority_s_edge).edge;
+							else
+								tri_edge_flip_index[index] = -1;
+						}
+					}
+					// find next edge with label h
+					edge_sym = sym_edges[highest_priority_s_edge];
+					for (int i = 0; i < 3; i++)
+					{
+						edge_sym = nxt(edge_sym);
+						if (h == edge_label[edge_sym.edge])
+						{
+							highest_priority_s_edge = get_index(edge_sym);
+							break;
+						}
+					}
+					num_iter--;
+				}
+			}
 		}
 		index += num_threads;
 	}
