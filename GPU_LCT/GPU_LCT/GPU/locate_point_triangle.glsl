bc9f6f8f726953b4a39a01a5566124c103d0bbd3
Unknown <mick3_70@hotmail.com>
2019-05-02 16:00:03 +0200

    Updated located_point_triangle

--- GPU_LCT/GPU_LCT/GPU/locate_point_triangle.glsl
+++ GPU_LCT/GPU_LCT/GPU/locate_point_triangle.glsl
@@ -85,152 +85,179 @@ layout (std140, binding = 0) uniform symedge_size
 //-----------------------------------------------------------
 // Access Functions
 //-----------------------------------------------------------
-
-SymEdge get_symedge(int index)
+void get_face(int face_i, out vec2 face_v[3])
 {
-	return sym_edges[index];
+	face_v[0] = point_positions[sym_edges[tri_symedges[face_i].x].vertex];
+	face_v[1] = point_positions[sym_edges[sym_edges[tri_symedges[face_i].x].nxt].vertex];
+	face_v[2] = point_positions[sym_edges[sym_edges[sym_edges[tri_symedges[face_i].x].nxt].nxt].vertex];
 }
 
-// symedge movement functions
-SymEdge nxt(SymEdge sym_edge)
-{
-	return get_symedge(sym_edge.nxt);
-}
 
-SymEdge rot(SymEdge sym_edge)
-{
-	return get_symedge(sym_edge.rot);
-}
-
-SymEdge prev(SymEdge s)
-{
-	return nxt(nxt(s));
-}
-SymEdge sym(SymEdge s)
-{
-	return rot(nxt(s));
-}
-
-int get_index(SymEdge s)
-{
-	return prev(s).nxt;
-}
+//-----------------------------------------------------------
+// SymEdge funcitons
+//-----------------------------------------------------------
 
 vec2 get_vertex(int index)
 {
 	return point_positions[index];
 }
 
-vec2 get_face_center(int face_i)
+SymEdge get_symedge(int index)
 {
-	vec2 face_v[3];
-	face_v[0] = point_positions[sym_edges[tri_symedges[face_i].x].vertex];
-	face_v[1] = point_positions[sym_edges[sym_edges[tri_symedges[face_i].x].nxt].vertex];
-	face_v[2] = point_positions[sym_edges[sym_edges[sym_edges[tri_symedges[face_i].x].nxt].nxt].vertex];
+	return sym_edges[index];
+}
 
-	return (face_v[0] + face_v[1] + face_v[2]) / 3.f; 
+int nxt(int edge)
+{
+	return sym_edges[edge].nxt;
+}
+
+SymEdge nxt(SymEdge s)
+{
+	return get_symedge(s.nxt);
+}
+
+int rot(int edge)
+{
+	return sym_edges[edge].rot;
+}
+
+SymEdge rot(SymEdge s)
+{
+	return get_symedge(s.rot);
+}
+
+int sym(int edge)
+{
+	return rot(nxt(edge));
+}
+
+SymEdge sym(SymEdge s)
+{
+	return rot(nxt(s));
+}
+
+int prev(int edge)
+{
+	return nxt(nxt(edge));
+}
+
+SymEdge prev(SymEdge s)
+{
+	return nxt(nxt(s));
 }
 
 //-----------------------------------------------------------
 // Math Functions
 //-----------------------------------------------------------
-
 vec2 project_point_on_line(vec2 point, vec2 a, vec2 b)
 {
-	vec2 ab = b - a;
+	vec2 ab = normalize(b - a);
 	vec2 ap = point - a;
-	return a + dot(ap, ab) / dot(ab, ab) * ab;
+	return a + dot(ap, ab) * ab;
 }
 
-bool point_intersects_line(vec2 p, vec2 a, vec2 b, float epsilon = EPSILON)
+bool point_ray_test(vec2 p1, vec2 r1, vec2 r2, float epsi = EPSILON)
 {
-	float dist = length(project_point_on_line(p, a, b) - p);
-	return abs(dist) < epsilon;
+	vec2 dist_vec = project_point_on_line(p1, r1, r2);
+	return abs(distance(dist_vec, p1)) < epsi ? true : false;
 }
 
-bool orientation(in vec2 p1 , in vec2 p2 , in vec2 p3)
+bool point_line_test(in vec2 p1, in vec2 s1, in vec2 s2, float epsi = EPSILON)
+{
+	vec2 dist_vec = project_point_on_line(p1, s1, s2);
+	if (!point_ray_test(p1, s1, s2, epsi))
+		return false;
+	vec2 v1 = s1 - p1;
+	vec2 v2 = s1 - s2;
+	float dot_p = dot(v1, v2);
+	if (dot_p < epsi * epsi)
+		return false;
+	if (dot_p > (dot(v2, v2) - epsi * epsi))
+		return false;
+
+	return true;
+}
+
+int orientation(vec2 p1, vec2 p2, vec2 p3)
 {
 	float val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
-	return (val > 0.0f) ? true : false;
+
+	if (val == 0.0f) return 0;
+	return (val > 0.0f) ? 1 : 2; // Clockwise : Counter Clockwise
 }
 
-bool line_line_test(in vec2 s1p1 , in vec2 s1p2, in vec2 s2p1, in vec2 s2p2)
+bool line_seg_intersection_ccw(vec2 p1, vec2 q1, vec2 p2, vec2 q2)
 {
-	bool hit = false;
-	bool o1 = orientation(s1p1, s1p2, s2p1);
-	bool o2 = orientation(s1p1, s1p2, s2p2);
-	bool o3 = orientation(s2p1, s2p2, s1p1);
-	bool o4 = orientation(s2p1, s2p2, s1p2);
+	int o1 = orientation(p1, q1, p2);
+	int o2 = orientation(p1, q1, q2);
+	int o3 = orientation(p2, q2, p1);
+	int o4 = orientation(p2, q2, q1);
 
 	if (o1 != o2 && o3 != o4)
-		hit = true;
-	else
-		hit = false;
+		return true;
 
-	return hit;
+	return false;
 }
 
 //-----------------------------------------------------------
 // Functions
 //-----------------------------------------------------------
-
-bool face_edges_contains_vertice(int face, int vertex)
+void oriented_walk(inout int curr_e, int point_i, out bool on_edge)
 {
-	SymEdge s = get_symedge(tri_symedges[face].x);
-	vec2 p = get_vertex(vertex);
-
-	vec2 t0 = get_vertex(s.vertex);
-	vec2 t1 = get_vertex(nxt(s).vertex);
-	vec2 t2 = get_vertex(prev(s).vertex);
-
-	if (point_intersects_line(p, t0, t1) || point_intersects_line(p, t1, t2) || point_intersects_line(p, t2, t0))
-	{
-		tri_ins_point_index[face] = vertex;
-		return true;
-	}
-
-	return false;
-}
-
-void oriented_walk_point(int curr_e, int goal_point_i)
-{
-	vec2 tri_cent;
-	vec2 goal_point = get_vertex(goal_point_i);
-	int i = 0;
-	while (i != 3)
-	{
-		if (face_edges_contains_vertice(get_symedge(curr_e).face, goal_point_i))
-			return;
-		
-		tri_cent = get_face_center(sym_edges[curr_e].face);
-		
+	bool done = false;
+	vec2 goal = point_positions[point_i];
+	int iter = 0;
+	on_edge = false;
+	while (!done) {
+		on_edge = false;
+		// Loop through triangles edges to check if point is on the edge 
+		for (int i = 0; i < 3; i++)
+		{
+			bool hit = false;
+			hit = point_line_test(goal,
+				point_positions[sym_edges[curr_e].vertex],
+				point_positions[sym_edges[sym_edges[curr_e].nxt].vertex]);
+			if (hit)
+			{
+				on_edge = true;
+				return;
+			}
+			curr_e = sym_edges[curr_e].nxt;
+		}
+		// calculate triangle centroid
+		vec2 tri_points[3];
+		get_face(sym_edges[curr_e].face, tri_points);
+		vec2 tri_cent = (tri_points[0] + tri_points[1] + tri_points[2]) / 3.0f;
 		// Loop through edges to see if we should continue through the edge
 		// to the neighbouring triangle 
 		bool line_line_hit = false;
-		for (i = 0; i < 3; i++)
+		for (int i = 0; i < 3; i++)
 		{
-			if (sym_edges[sym_edges[curr_e].nxt].rot == -1)
-			{
-				curr_e = sym_edges[curr_e].nxt;
-				continue;
-			}
-
-			line_line_hit = line_line_test(
+			line_line_hit = line_seg_intersection_ccw(
 				tri_cent,
-				goal_point,
+				goal,
 				point_positions[sym_edges[curr_e].vertex],
 				point_positions[sym_edges[sym_edges[curr_e].nxt].vertex]);
 
 			if (line_line_hit)
 			{
-				curr_e = sym_edges[sym_edges[curr_e].nxt].rot;
 				break;
 			}
 			curr_e = sym_edges[curr_e].nxt;
 		}
+
+		if (line_line_hit)
+		{
+			curr_e = nxt(sym_edges[sym_edges[curr_e].nxt].rot); // sym
+		}
+		else
+		{
+			return;
+		}
 	}
-	return;
 }
+
 
 // Each thread represents one point
 
@@ -240,6 +267,15 @@ void main(void)
 	int num_threads = int(gl_NumWorkGroups.x * gl_WorkGroupSize.x);
 	int index = int(gid);
 
-	if (index < point_positions.length() && point_inserted[index] == 0)
-		oriented_walk_point(point_tri_index[index], index);
+	while (index < point_positions.length())
+	{
+		if (point_inserted[index] == 0)
+		{
+			int curr_e = point_tri_index[index];
+			bool on_edge = false;
+			oriented_walk(curr_e, index, on_edge);
+			point_tri_index[index] = curr_e;
+			tri_ins_point_index[sym_edges[curr_e].face] = index;
+		}
+	}
 }
