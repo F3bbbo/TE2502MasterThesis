12bf2d1036a91589f14f9a27a959a33a38f01715
Simon <gummessonsimon@gmail.com>
2019-05-06 11:22:21 +0200

    Add GCMesh first test

--- GPU_LCT/GPU_LCT/TestFunctions.hpp
+++ GPU_LCT/GPU_LCT/TestFunctions.hpp
@@ -5,3 +5,6 @@
 #include "Timer.hpp"
 
 void test_range(glm::ivec2 start_resolution, int iterations, glm::ivec2 start_dims, glm::ivec2 dim_increase, glm::ivec2 start_obstacles, glm::ivec2 obstacle_increase, bool build_lct);
+
+void first_test(glm::vec2 static_obstacle_amount, int iterations);
+
