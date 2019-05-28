e30e2d28decd0704b7ff2e483a42f6208b8d2c37
Simon <gummessonsimon@gmail.com>
2019-05-28 21:07:07 +0200

    Add standard deviation to first test script

--- GPU_LCT/GPU_LCT/pyplot/first_test_plot.py
+++ GPU_LCT/GPU_LCT/pyplot/first_test_plot.py
@@ -1,29 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 import sys
 
+# process input arguments
 if (len(sys.argv) != 4):
+    print("provide three arguments [G,C] [START_NUM] [END_NUM]")
     sys.exit(1)
 
 filename = "first_test_";
 
-if (sys.argv[1] == "GPU"):
+if (sys.argv[1] == "G" or sys.argv[1] == "g"):
     filename += "GPU-";
-elif (sys.argv[1] == "CPU"):
+elif (sys.argv[1] == "C" or sys.argv[1] == "c"):
     filename += "CPUGPU-";
 else:
-    sys.exit(0)
+    print("First argument has to be [Gg] or [Cc]]")
+    sys.exit(1)
+
+try:
+    int(sys.argv[2])
+except ValueError:
+    print("Second argument has to be an integer")
+    sys.exit(1)
+
+try:
+    int(sys.argv[3])
+except ValueError:
+    print("third argument has to be an integer")
+    sys.exit(1)
+# end processing of input
 
 CDT = int(sys.argv[2])
 LCT = int(sys.argv[3])
-
-if (not isinstance(CDT, int) or not isinstance(LCT, int)):
-    sys.exit(2)
-
 filename += str(CDT) + '-' + str(LCT) + ".txt"
 
 Matrix = None
-sums = [0] * 2
 iterations = 0;
 increase_iterations = 0;
 
@@ -34,41 +46,56 @@ with open(filename, 'r') as file:
     iterations = int(metadata[0])
     increase_iterations = int(metadata[1])
 
-    Matrix = [[0 for x in range(increase_iterations)] for y in range(3)]
+    Matrix = [[0 for x in range(increase_iterations)] for y in range(5)]
+    results = [[0 for x in range(iterations)] for y in range(2)]
     counter = 0
     completed_iter = 0
     for line in file:
         if (counter == 0):
             num_obstacles = int(line)
             counter += 1
-        elif (counter == 11):
-            sums[0] /= iterations
-            sums[1] /= iterations
-            Matrix[0][completed_iter] = sums[0]
-            Matrix[1][completed_iter] = sums[1]
-            Matrix[2][completed_iter] = num_obstacles
-            sums[0] = 0
-            sums[1] = 0
+        elif (counter > iterations):
+            # compute CDT mean
+            for result in results[0]:
+                Matrix[0][completed_iter] += result
+            Matrix[0][completed_iter] /= iterations
 
+            # compute CDT standard deviation
+            for result in results[0]:
+                Matrix[1][completed_iter] += (result - Matrix[0][completed_iter]) ** 2
+            Matrix[1][completed_iter] = math.sqrt(Matrix[1][completed_iter] / iterations)
+
+            # compute LCT mean
+            for result in results[1]:
+                Matrix[2][completed_iter] += result
+            Matrix[2][completed_iter] /= iterations
+
+            # compute LCT standard deviation
+            for result in results[1]:
+                Matrix[3][completed_iter] += (result - Matrix[2][completed_iter]) ** 2
+            Matrix[3][completed_iter] = math.sqrt(Matrix[3][completed_iter] / iterations)
+
+            Matrix[4][completed_iter] = num_obstacles
             counter = 0
+            results = [[0 for x in range(iterations)] for y in range(2)]
             completed_iter += 1
         else:
             l = line.split(',')
-            sums[0] += int(l[1])
-            sums[1] += int(l[2][:-1])
+            results[0][counter - 1] += int(l[1])
+            results[1][counter - 1] += int(l[2][:-1])
             counter += 1
 
-
+# plotting starts here
 fig, ax = plt.subplots()
 
 ind = np.arange(len(Matrix[0]))    # the x locations for the groups
 width = 0.35         # the width of the bars
-p1 = ax.bar(ind, Matrix[0], width, bottom=0)
-p2 = ax.bar(ind + width, Matrix[1], width, bottom=0)
+p1 = ax.bar(ind, Matrix[0], width, bottom=0, yerr=Matrix[1])
+p2 = ax.bar(ind + width, Matrix[2], width, bottom=0, yerr=Matrix[3])
 
 ax.set_title('Time to build CDT and LCT')
 ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(Matrix[2])
+ax.set_xticklabels(Matrix[4])
 
 ax.legend((p1[0], p2[0]), ('CDT', 'LCT'))
 ax.set_ylabel('milliseconds')
