65fd1efa2fdfb1d18f601b5aa6401e635f3231b3
Simon <gummessonsimon@gmail.com>
2019-06-03 15:34:39 +0200

    Add first test wip

--- GPU_LCT/CPU_LCT/main.cpp
+++ GPU_LCT/CPU_LCT/main.cpp
@@ -10,66 +10,72 @@
 
 #include <iostream>
 #include <vector>
+#include <glm/glm.hpp>
 
 #include "../GPU_LCT/Timer.hpp"
+#include "../GPU_LCT/TestMap.hpp"
+#include "../GPU_LCT/Log.hpp"
+
+void first_test(glm::ivec2 obstacle_amount, glm::ivec2 obstacle_increase, int increase_iterations, int iterations);
+
 int main()
 {
 	// Activate license:
 	const char* licfile = "triplannerlic.txt";
-	tp_verify_license_file ( licfile ); // first check if license file is there
-	tp_activate ( licfile ); // ok, load and activate
-
-	// Path queries with clearance require a DmLct:
-	// Here we create one with precision 0.001:
-	tpLct* lct = tp_lct_newref ( 0.001f );
-
-	// Initialize triangulation with domain and border expanded by 0.02:
-	tp_lct_init ( lct, -100, -100, 100, 100, 0);
-
-	// Now insert an obstacle:
-	//tpPolygon* obs = tp_polygon_newref();
-	//tp_polygon_read ( obs, "-3 3  -3 -3  3 -3  3 3" ); // square of side 6 centered at (0,0)
-	//int id = tp_lct_insert_polygon ( lct, obs );
-	//tp_lct_remove_polygon ( lct, id ); // comment this line to see the extra square
-
-
-	// Obstacles can also be inserted directly from any array type of class:
-	Timer t;
-	t.start();
-
-	int id;
-	for (int i = 0; i < 1000; i++)
-	{
-		std::vector<float> poly;
-		poly.push_back(-3.f + i * 0.01f); poly.push_back(-2.f + i * 0.01f);
-		poly.push_back(3.f + i * 0.01f); poly.push_back(-2.f + i * 0.01f);
-		poly.push_back(0.f + i * 0.01f); poly.push_back(1.f + i * 0.01f);
-		id = tp_lct_insert_polygonfv(lct, poly.data(), poly.size() / 2, TpClosedPolygon);
-	}
-	t.stop();
+	tp_verify_license_file(licfile); // first check if license file is there
+	tp_activate(licfile); // ok, load and activate
 	
-	std::cout << t.elapsed_time() << std::endl;
+	//first_test();
 
-	tp_lct_remove_polygon ( lct, id ); // comment out this removal and you will see the new triangle inserted
-
-	// Obstacles can be polygonal lines as well, here are many examples:
-	//# define INSERT(s) tp_polygon_read ( obs, s ); tp_lct_insert_polygon ( lct, obs )
-	//INSERT ( "polyline -9 1  -9 7  -9 4  -5 4  -5 7  -5 1" ); // H
-	//INSERT ( "polyline -1 1  -3 1  -3 5  -1 5  -1 3  -3 3" ); // e
-	//INSERT ( "polyline 1 1 1 7" ); // l
-	//INSERT ( "polyline 4 1 4 7" ); // l
-	//INSERT ( "polygon 6 1  8 1  8 5  6 5" ); // o
-	//INSERT ( "polyline -9 -1  -9 -7  -7 -5  -5 -7  -5 -1" ); // W
-	//INSERT ( "polygon -3 -7  -1 -7  -1 -3  -3 -3" ); // o
-	//INSERT ( "polyline 1 -7  1 -3  1 -4  2 -4" ); // r
-	//INSERT ( "polyline 4 -7  4 -1" ); // l
-	//INSERT ( "polygon 6 -7  8 -7  8 -1  8 -3  6 -3" ); // d
-	//# undef INSERT
-
-	//tp_polygon_unref ( obs ); // polygon object no more used: release it
-
-	// Clean up:
-	tp_lct_unref ( lct );
 	getchar();
 	return 0;
+}
+
+void first_test(glm::ivec2 obstacle_amount, glm::ivec2 obstacle_increase, int increase_iterations, int iterations)
+{
+	std::vector<std::vector<long long>> build_times;
+	build_times.resize(increase_iterations);
+
+	std::vector<int> vertice_counts;
+	vertice_counts.resize(increase_iterations);
+	for (int iter = 0; iter < increase_iterations; iter++)
+	{
+		TestMap test_map;
+		test_map.set_map_size({ 45, 45 }, { -45, -45 });
+		test_map.set_num_obsticles(obstacle_amount + obstacle_increase * iter);
+		test_map.set_static_quota(1.f);
+		auto gpu_frame = test_map.get_GPU_frame();
+
+		auto polygons = test_map.get_CPU_static_obstacles();
+
+		std::pair<std::vector<glm::vec2>, std::vector<glm::ivec2>> data = test_map.get_GPU_obstacles();
+
+		vertice_counts[iter] = data.first.size();
+		// test Kallmanns solution
+		for (int i = 0; i < iterations; i++)
+		{
+			tpLct* lct = tp_lct_newref(0.001f);
+			tp_lct_init(lct, -45, -45, 45, 45, 0);
+
+			// Only build CDT
+			tp_lct_mode(lct, TpRefMode::TpRefGlobal, TpRemMode::TpRemFull);
+			Timer timer;
+
+			timer.start();
+			for (std::vector<glm::vec2>& polygon : polygons)
+				tp_lct_insert_polygonfv(lct, (float*)polygon.data(), polygon.size(), TpClosedPolygon);
+			timer.stop();
+			build_times[iter].push_back(timer.elapsed_time());
+
+			timer.start();
+			tp_lct_refine(lct);
+			timer.stop();
+			build_times[iter].push_back(timer.elapsed_time());
+
+			tp_lct_unref(lct);
+			LOG_ND("First Test iteration: " + std::to_string(i + 1) + '\n');
+		}
+	}
+
+	build_times;
 }
