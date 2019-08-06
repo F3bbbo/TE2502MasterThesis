4b20fe785fa10ef35f55ed0c01284ef8459eee9f
Unknown <mick3_70@hotmail.com>
2019-08-06 08:45:27 +0200

    Change of map size slightly to work for GPU v2 tests

--- GPU_LCT/GPU_LCT/TestParameters.hpp
+++ GPU_LCT/GPU_LCT/TestParameters.hpp
@@ -2,7 +2,7 @@
 #ifndef TEST_PARAMETERS_H
 #define TEST_PARAMETERS_H
 
-#define TEST_MAP_SIZE_X 49.0f
-#define TEST_MAP_SIZE_Y 49.0f
+#define TEST_MAP_SIZE_X 49.99f
+#define TEST_MAP_SIZE_Y 49.99f
 
 #endif
