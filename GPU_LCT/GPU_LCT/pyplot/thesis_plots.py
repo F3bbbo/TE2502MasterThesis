8da04d7267f0768182c1032c3870b3e08ced4efe
Unknown <mick3_70@hotmail.com>
2019-08-20 16:00:42 +0200

    Get result from file

--- GPU_LCT/GPU_LCT/pyplot/thesis_plots.py
+++ GPU_LCT/GPU_LCT/pyplot/thesis_plots.py
@@ -5,7 +5,7 @@ import sys
 import re
 import os
 
-num_args = len(sys.argv)
+num_args = len(sys.argv) - 1
 # arguments:
 # -plots 1 2 3 4 ...
 # -in locations
@@ -16,38 +16,92 @@ def print_help():
         1 : First test plot
         2 : Second test plot
         3 : Third test plot
-    -w / -workfolder "folder" set the workspace folder
-    -i / -infolder "folder" set folder where input is
-    -o / -outfolder "folder" set folder where output will go
+    -w / -workfolder "path" set the workspace folder
+    -i / -infolder "path" set folder where input is
+    -o / -outfolder "path" set folder where output will go
+    -k / kallmannfolder "path" set the folder where Kallann test results are
     """
     print(options)
 def all_plots():
     return dict({1 : True, 2 : True, 3 : True})
 
 plots = all_plots()
-in_folder = ""
+in_folder = "..\\Output files"
 out_folder = ""
 work_folder = ""
+kallmann_folder = "..\\..\\CPU_LCT\\Output files"
 out_file_name = "Result_plots"
 
-def abs_path(fileName, is_input = True):
+def abs_path(fileName, is_gpu = True, is_input = True):
     global work_folder
     global in_folder
     global out_folder
-    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-    if(is_input):
-        rel_path = os.path.join(in_folder,fileName)
+    global kallmann_folder
+    script_dir = os.path.dirname(os.path.abspath(__file__)) #<-- absolute dir the script is in
+    #print("script dir: " + script_dir)
+    if(is_gpu):
+        if(is_input):
+            rel_path = os.path.join(in_folder,fileName)
+        else:
+            rel_path = os.path.join(out_folder, fileName)
+        rel_path = os.path.join(work_folder, rel_path)
+        abs_file_path = os.path.join(script_dir, rel_path)
     else:
-        rel_path = os.path.join(out_folder, fileName)
-    rel_path = os.path.join(work_folder, rel_path)
-    abs_file_path = os.path.join(script_dir, rel_path)
+        abs_file_path = os.path.join(script_dir, kallmann_folder)
+        abs_file_path = os.path.join(abs_file_path, fileName)
     return abs_file_path;
+
+def get_results_from_file(file_name):
+    with open(file_name, "r") as file:
+        y_groups = file.readline().split(',')
+        meta_data = file.readline().split(',')
+        iterations = int(meta_data[0])
+        increase_iterations = int(meta_data[1])
+        # Create base of return values
+        y_labels = dict()
+        y_labels[y_groups[0]] = list()
+        y_labels[y_groups[1]] = list()
+        x_labels = list()
+        std_dev = dict()
+        std_dev[y_groups[0]] = list()
+        std_dev[y_groups[1]] = list()
+        counter = 0
+        for line in file:
+            if(counter == 0):
+                num_vertices = int(line)
+                iter_values = dict()
+                iter_values[y_groups[0]] = list()
+                iter_values[y_groups[1]] = list()
+
+                counter += 1
+            elif(counter > iterations):
+                # add the average values to y_labels ans std_dev
+                x_labels.append(num_vertices)
+                # Calculate average and std of first group
+                y_labels[y_groups[0]].append(np.mean(iter_values[y_groups[0]]))
+                std_dev[y_groups[0]].append(np.std(iter_values[y_groups[0]]))
+
+                # Calculate average and std of second group
+                y_labels[y_groups[1]].append(np.mean(iter_values[y_groups[1]]))
+                std_dev[y_groups[1]].append(np.std(iter_values[y_groups[1]]))
+
+                # Reset counter var
+                counter = 0
+            else:
+                l = line.split(',')
+                iter_values[y_groups[0]].append(float(l[0]))
+                iter_values[y_groups[1]].append(float(l[1]))
+                counter += 1
+
+    return y_labels, y_groups,  x_labels, std_dev
+
 
 
 def process_parameter(start_i):
     global plots
     global in_folder
     global out_folder
+    global num_args
     if(start_i < num_args):
         next_i = start_i + 1
         reg_match = re.match("-[a-z]*", sys.argv[start_i])
@@ -66,11 +120,23 @@ def process_parameter(start_i):
                 if(zero_found):
                     plots = all_plots()
             elif(argument == "i" or argument == "infolder"):
-                in_folder = sys.argv[next_i]
+                in_folder =  sys.argv[next_i]
                 next_i += 1
+                while(next_i <= num_args and sys.argv[next_i] is not "-"):
+                    in_folder += " " + sys.argv[next_i]
+                    next_i += 1
             elif(argument == "o" or argument == "outfolder"):
                 out_folder = sys.argv[next_i]
                 next_i += 1
+                while(next_i <= num_args and sys.argv[next_i] is not "-"):
+                    out_folder += " " + sys.argv[next_i]
+                    next_i += 1
+            elif(argument == "w" or argument == "workfolder"):
+                work_folder =  sys.argv[next_i]
+                next_i += 1
+                while(next_i <= num_args and sys.argv[next_i] is not "-"):
+                    out_folder += " " + sys.argv[next_i]
+                    next_i += 1
             else:
                 print_help()
                 next_i = -1
@@ -84,12 +150,18 @@ def process_parameter(start_i):
 curr_i = 1
 while (curr_i != -1):
     curr_i = process_parameter(curr_i)
+# Test code
 #print_help()
 #print(plots)
+#print("-plot"[0] is "-")
 
 # First plot
 if(plots.get(1) is not None):
-
+    first_CG_results = get_results_from_file(abs_path("first_test_CPUGPU-9-3600-v2.txt"))
+    print(first_CG_results)
+    first_CG_file = open(abs_path("first_test_CPUGPU-9-3600-v2.txt"), "r")
+    first_G_file = open(abs_path("first_test_CPUGPU-100-90000-v2.txt"), "r")
+    first_Kall_file = open(abs_path("first_test_CPU-100-90000-v0.txt", False), "r")
 # Second plot
 
 
